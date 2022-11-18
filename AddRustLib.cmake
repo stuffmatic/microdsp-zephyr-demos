9f2a3e4ddcdb3c1c927f3c250c5bd28c54ce3a08
Per <per@stuffmatic.com>
2022-11-18 14:05:23 +0100

    select cargo profile based on zephyr opt level

--- AddRustLib.cmake
+++ AddRustLib.cmake
@@ -5,19 +5,26 @@ function(
   include_path
 )
 
-# TODO:
-# * arch selection
-# * debug/release
-
 # TODO: replace - with _
 set(LIB_NAME ${crate_name})
 
 # TODO: replace - with _
 set(LIB_FILENAME lib${crate_name}.a)
 
-# Select a cargo build profile
-set(CARGO_PROFILE debug)
+# Select a cargo build profile, if not already set
+if (NOT DEFINED CARGO_PROFILE)
+  set(CARGO_PROFILE release)
+  if (DEFINED CONFIG_NO_OPTIMIZATIONS)
+    set(CARGO_PROFILE debug)
+  endif()
+endif()
 
+# Set args to pass to cargo build depending on build profile
+if (${CARGO_PROFILE} STREQUAL "debug")
+  set(CARGO_ARGS "")
+else()
+  set(CARGO_ARGS --release)
+endif()
 
 # Select a cargo build target suitable for the current CPU
 if(DEFINED CONFIG_CPU_CORTEX_M0)
@@ -42,19 +49,19 @@ endif()
 
 set(LIB_PATH ${crate_root}/target/${CARGO_TARGET}/${CARGO_PROFILE}/${LIB_FILENAME})
 
-include(CMakePrintHelpers)
+add_custom_command(
+  OUTPUT ${LIB_FILENAME}
+  BYPRODUCTS ${LIB_FILENAME}
+  COMMAND cargo build --target ${CARGO_TARGET} ${CARGO_ARGS}
+  WORKING_DIRECTORY ${crate_root}
+  COMMENT "Building rust library '${crate_name}' target='${CARGO_TARGET}' profile='${CARGO_PROFILE}' args='${CARGO_ARGS}'"
+  VERBATIM
+)
 
 add_library(${LIB_NAME} INTERFACE ${LIB_FILENAME})
 target_include_directories(${LIB_NAME} INTERFACE ${include_path})
 target_link_libraries(${LIB_NAME} INTERFACE ${LIB_PATH})
 target_link_libraries(app PRIVATE ${LIB_NAME})
 
-add_custom_command(
-  OUTPUT ${LIB_FILENAME}
-  COMMAND cargo build --target ${CARGO_TARGET}
-  WORKING_DIRECTORY ${crate_root}
-  COMMENT "Building rust library '${crate_name}' target='${CARGO_TARGET}' args='${CARGO_ARGS}'"
-  VERBATIM
-)
 
 endfunction()
