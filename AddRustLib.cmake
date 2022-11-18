a105c78a98269d96b4db0cff56b83403853f6c35
Per <per@stuffmatic.com>
2022-11-18 13:12:23 +0100

    rust lib build improvements

--- AddRustLib.cmake
+++ AddRustLib.cmake
@@ -15,9 +15,30 @@ set(LIB_NAME ${crate_name})
 # TODO: replace - with _
 set(LIB_FILENAME lib${crate_name}.a)
 
-# TODO: Select based on zephyr build settings
+# Select a cargo build profile
 set(CARGO_PROFILE debug)
-set(CARGO_TARGET thumbv7em-none-eabi)
+
+
+# Select a cargo build target suitable for the current CPU
+if(DEFINED CONFIG_CPU_CORTEX_M0)
+  set(CARGO_TARGET thumbv6m-none-eabi)
+elseif(DEFINED CONFIG_CPU_CORTEX_M3)
+  set(CARGO_TARGET thumbv7em-none-eabi)
+elseif(DEFINED CONFIG_CPU_CORTEX_M33)
+  if(DEFINED CPU_HAS_FPU)
+  set(CARGO_TARGET thumbv8m.main-none-eabihf)
+  else()
+  set(CARGO_TARGET thumbv8m.main-none-eabih)
+  endif()
+elseif(DEFINED CONFIG_CPU_CORTEX_M4 OR DEFINED CONFIG_CPU_CORTEX_M7)
+  if(DEFINED CPU_HAS_FPU)
+  set(CARGO_TARGET thumbv7em-none-eabihf)
+  else()
+  set(CARGO_TARGET thumbv7em-none-eabi)
+  endif()
+else()
+  message(FATAL_ERROR "Failed to set a cargo build target for CPU type")
+endif()
 
 set(LIB_PATH ${crate_root}/target/${CARGO_TARGET}/${CARGO_PROFILE}/${LIB_FILENAME})
 
