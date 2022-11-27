50cd7c4b88a190af3b3cffc709eed735a102d163
Per <per@stuffmatic.com>
2022-11-27 13:53:20 +0100

    fix wm8904 i2c read/write bugs

--- src/codec.c
+++ src/codec.c
@@ -12,7 +12,6 @@ struct register_write
     uint16_t data;
 };
 
-// Writes data to a wmXXXX codec register
 int write_reg(uint8_t device_addr, uint16_t reg_addr, uint16_t data)
 {
     __ASSERT(reg_addr <= 0x7f, "invalid codec register address"); // 7 bit register address
@@ -20,7 +19,7 @@ int write_reg(uint8_t device_addr, uint16_t reg_addr, uint16_t data)
     const uint16_t reg_and_address = (reg_addr << 9) | data;
 
     uint8_t payload[2] = {
-        (reg_and_address >> 8) & 0xff, // msb
+        (reg_and_address & 0xff) >> 8, // msb
         (reg_and_address >> 0) & 0xff  // lsb
     };
     int write_rc = i2c_write(i2c_dev, payload, 2, device_addr);
@@ -28,18 +27,28 @@ int write_reg(uint8_t device_addr, uint16_t reg_addr, uint16_t data)
     return write_rc;
 }
 
-uint32_t read_reg(uint8_t device_addr, uint16_t reg_addr)
+uint32_t wm8904_read_reg(uint8_t reg_addr)
 {
-    __ASSERT(reg_addr <= 0x7f, "invalid codec register address"); // 7 bit register address
-    const uint16_t reg_and_address = (reg_addr << 9);
-    uint8_t read_data[2] = {
-        (reg_and_address >> 8) & 0xff, // msb
-        (reg_and_address >> 0) & 0xff  // lsb
-    };
-    int read_rc = i2c_read(i2c_dev, read_data, 2, device_addr);
+    uint8_t write_buf[1] = { reg_addr };
+    uint8_t read_buf[2] = { 0, 0 };
+    int read_rc = i2c_write_read(i2c_dev, WM8904_ADDRESS, write_buf, 1, read_buf, 2);
     printk("read_reg result %d\n", read_rc);
-    uint32_t result = ((read_data[0] << 8) & 0xff00) | read_data[1];
+    uint32_t result = ((read_buf[0] << 8) & 0xff00) | read_buf[1];
     return result;
+}
+
+int wm8904_write_reg(uint8_t reg_addr, uint16_t data)
+{
+    uint8_t write_buf[3] = {
+        reg_addr,
+        (data & 0xff00) >> 8, // msb
+        data & 0xff  // lsb
+    };
+    uint8_t read_buf[2] = { 0, 0 };
+    // int write_rc = i2c_write(i2c_dev, payload, 3, WM8904_ADDRESS);
+    int write_rc = i2c_write_read(i2c_dev, WM8904_ADDRESS, write_buf, 3, read_buf, 2);
+    printk("write_reg result %d\n", write_rc);
+    return write_rc;
 }
 
 void init_wm8758b_codec()
@@ -243,119 +252,118 @@ void init_wm8904_codec()
         // Note that, if it cannot be assured that MCLK is present when accessing
         // the register map, then it is required to set CLK_SYS_ENA = 0 to
         // ensure correct operation.
-        write_reg(WM8904_ADDRESS, WM8904_SW_RESET_AND_ID, 0);
+        wm8904_write_reg(WM8904_SW_RESET_AND_ID, 0);
 
         // Read device id to make sure the device is present
-        uint32_t device_id = read_reg(WM8904_ADDRESS, WM8904_SW_RESET_AND_ID);
+        uint32_t device_id = wm8904_read_reg(WM8904_SW_RESET_AND_ID);
         __ASSERT(device_id == 0x8904, "Failed to read wm8904 device id");
-        k_msleep(1);
 
         // Perform start-up sequence
         // 0.
         int ISEL = 0b10;
-        write_reg(WM8904_ADDRESS, WM8904_BIAS_CONTROL_0, ISEL << 2);
+        wm8904_write_reg(WM8904_BIAS_CONTROL_0, ISEL << 2);
         k_msleep(100);
 
         // 1.
         int VMID_BUF_ENA = 1;
         int VMID_RES = 0b11;
         int VMID_ENA = 1;
-        write_reg(WM8904_ADDRESS, WM8904_VMID_CONTROL_0, (VMID_ENA << 0) | (VMID_RES << 1) | (VMID_BUF_ENA << 6));
+        wm8904_write_reg(WM8904_VMID_CONTROL_0, (VMID_ENA << 0) | (VMID_RES << 1) | (VMID_BUF_ENA << 6));
         k_msleep(5);
 
         // 2.
         VMID_RES = 0b01;
-        write_reg(WM8904_ADDRESS, WM8904_VMID_CONTROL_0, (VMID_ENA << 0) | (VMID_RES << 1) | (VMID_BUF_ENA << 6));
+        wm8904_write_reg(WM8904_VMID_CONTROL_0, (VMID_ENA << 0) | (VMID_RES << 1) | (VMID_BUF_ENA << 6));
         k_msleep(1);
 
         // 3.
-        write_reg(WM8904_ADDRESS, WM8904_BIAS_CONTROL_0, (ISEL << 2) | (1 << 0));
+        wm8904_write_reg(WM8904_BIAS_CONTROL_0, (ISEL << 2) | (1 << 0));
         k_msleep(1);
 
         // 4.
-        write_reg(WM8904_ADDRESS, WM8904_POWER_MANAGEMENT_2, (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_POWER_MANAGEMENT_2, (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 5.
-        write_reg(WM8904_ADDRESS, WM8904_POWER_MANAGEMENT_3, (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_POWER_MANAGEMENT_3, (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 6.
-        write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_2, (1 << 1));
+        wm8904_write_reg(WM8904_CLOCK_RATES_2, (1 << 1));
         k_msleep(1);
 
         // 7.
-        write_reg(WM8904_ADDRESS, WM8904_POWER_MANAGEMENT_6, (1 << 3) | (1 << 2));
+        wm8904_write_reg(WM8904_POWER_MANAGEMENT_6, (1 << 3) | (1 << 2));
         k_msleep(1);
 
         // 10.
-        write_reg(WM8904_ADDRESS, WM8904_CHARGE_PUMP_0, (1 << 0));
+        wm8904_write_reg(WM8904_CHARGE_PUMP_0, (1 << 0));
         k_msleep(1);
 
         // 12.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, (1 << 4) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, (1 << 4) | (1 << 0));
         k_msleep(1);
 
         // 13.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, (1 << 4) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, (1 << 4) | (1 << 0));
         k_msleep(1);
 
         // 14.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, (1 << 5) | (1 << 4) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, (1 << 5) | (1 << 4) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 15.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, (1 << 5) | (1 << 4) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, (1 << 5) | (1 << 4) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 16.
-        write_reg(WM8904_ADDRESS, WM8904_DC_SERVO_0, (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_DC_SERVO_0, (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 17.
-        write_reg(WM8904_ADDRESS, WM8904_DC_SERVO_1, (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4));
+        wm8904_write_reg(WM8904_DC_SERVO_1, (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4));
         k_msleep(1);
 
         // 19.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, (1 << 6) | (1 << 5) | (1 << 4) | (1 << 2) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, (1 << 6) | (1 << 5) | (1 << 4) | (1 << 2) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 20.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, (1 << 6) | (1 << 5) | (1 << 4) | (1 << 2) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, (1 << 6) | (1 << 5) | (1 << 4) | (1 << 2) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 21.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // 22.
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, (1 << 7) | (1 << 6) | (1 << 5) | (1 << 4) | (1 << 3) | (1 << 2) | (1 << 1) | (1 << 0));
         k_msleep(1);
 
         // Pll config
         {
-            write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_1, 0b001); // ENABLE FLL, FRACN_ENA false
-            write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_2, (9 << 8));
-            write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_3, 0x0);
-            write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_4, (72 << 5));
-            write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_5, 0b01); // use bitclock as FLL reference
+            wm8904_write_reg(WM8904_FLL_CONTROL_1, 0b001); // ENABLE FLL, FRACN_ENA false
+            wm8904_write_reg(WM8904_FLL_CONTROL_2, (9 << 8));
+            wm8904_write_reg(WM8904_FLL_CONTROL_3, 0x0);
+            wm8904_write_reg(WM8904_FLL_CONTROL_4, (72 << 5));
+            wm8904_write_reg(WM8904_FLL_CONTROL_5, 0b01); // use bitclock as FLL reference
 
             k_msleep(5);
 
             // wm8904_write_register(WM8904_CLOCK_RATES_1, WM8904_CLK_SYS_RATE(3) | WM8904_SAMPLE_RATE(5));
-            // write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_1, 0b101 | (0b11 << 10)); // sysclk = 256fs, fs =~ 44.1 khz
+            // wm8904_write_reg(WM8904_CLOCK_RATES_1, 0b101 | (0b11 << 10)); // sysclk = 256fs, fs =~ 44.1 khz
             // wm8904_write_register(WM8904_CLOCK_RATES_0, 0x0000);
-            // write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_0, 0b0);
+            // wm8904_write_reg(WM8904_CLOCK_RATES_0, 0b0);
             // wm8904_write_register(WM8904_CLOCK_RATES_2,
             //           		     WM8904_SYSCLK_SRC | WM8904_CLK_SYS_ENA | WM8904_CLK_DSP_ENA);
-            // write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 14)); // sysclk src = pll, disable sysclock
+            // wm8904_write_reg(WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 14)); // sysclk src = pll, disable sysclock
             // wm8904_write_register(WM8904_AUDIO_INTERFACE_1, WM8904_BCLK_DIR | WM8904_AIF_FMT_I2S);
             const int word_len = (0b00 << 2); // 16 bit
-            write_reg(WM8904_ADDRESS, WM8904_AUDIO_INTERFACE_1, 0b10 | word_len);
+            wm8904_write_reg(WM8904_AUDIO_INTERFACE_1, 0b10 | word_len);
         }
 
         // enable sysclk and set sysclk src to to fll
-        write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 2) | (1 << 14));
+        wm8904_write_reg(WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 2) | (1 << 14));
         k_msleep(1);
     }
 }
@@ -369,70 +377,70 @@ void init_wm8904_codec_old()
         // Note that, if it cannot be assured that MCLK is present when accessing
         // the register map, then it is required to set CLK_SYS_ENA = 0 to
         // ensure correct operation.
-        write_reg(WM8904_ADDRESS, WM8904_SW_RESET_AND_ID, 0);
+        wm8904_write_reg(WM8904_SW_RESET_AND_ID, 0);
 
-        uint32_t device_id = read_reg(WM8904_ADDRESS, WM8904_SW_RESET_AND_ID);
+        uint32_t device_id = wm8904_read_reg(WM8904_SW_RESET_AND_ID);
         __ASSERT(device_id == 0x8904, "Failed to read wm8904 device id");
 
         // wm8904_write_register(WM8904_BIAS_CONTROL_0, WM8904_ISEL_HP_BIAS);
-        write_reg(WM8904_ADDRESS, WM8904_BIAS_CONTROL_0, 0b1000);
+        wm8904_write_reg(WM8904_BIAS_CONTROL_0, 0b1000);
 
         // wm8904_write_register(WM8904_VMID_CONTROL_0, WM8904_VMID_BUF_ENA |
         //     					 WM8904_VMID_RES_FAST | WM8904_VMID_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_VMID_CONTROL_0, 0b1000111);
-        uint32_t test_read = read_reg(WM8904_ADDRESS, WM8904_VMID_CONTROL_0);
+        wm8904_write_reg(WM8904_VMID_CONTROL_0, 0b1000111);
+        uint32_t test_read = wm8904_read_reg(WM8904_VMID_CONTROL_0);
         __ASSERT(test_read == 0b1000, "read/write mismatch");
         k_msleep(5);
         // wm8904_write_register(WM8904_VMID_CONTROL_0, WM8904_VMID_BUF_ENA |
         //    					 WM8904_VMID_RES_NORMAL | WM8904_VMID_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_VMID_CONTROL_0, 0b1000011);
+        wm8904_write_reg(WM8904_VMID_CONTROL_0, 0b1000011);
         // wm8904_write_register(WM8904_BIAS_CONTROL_0, WM8904_ISEL_HP_BIAS | WM8904_BIAS_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_BIAS_CONTROL_0, 0b11001);
+        wm8904_write_reg(WM8904_BIAS_CONTROL_0, 0b11001);
         // wm8904_write_register(WM8904_POWER_MANAGEMENT_0, WM8904_INL_ENA | WM8904_INR_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_POWER_MANAGEMENT_0, 0b11);
+        wm8904_write_reg(WM8904_POWER_MANAGEMENT_0, 0b11);
         // wm8904_write_register(WM8904_POWER_MANAGEMENT_2, WM8904_HPL_PGA_ENA | WM8904_HPR_PGA_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_POWER_MANAGEMENT_2, 0b11);
+        wm8904_write_reg(WM8904_POWER_MANAGEMENT_2, 0b11);
         // wm8904_write_register(WM8904_DAC_DIGITAL_1, WM8904_DEEMPH(0));
-        write_reg(WM8904_ADDRESS, WM8904_DAC_DIGITAL_1, 0b0);
+        wm8904_write_reg(WM8904_DAC_DIGITAL_1, 0b0);
         // wm8904_write_register(WM8904_ANALOGUE_OUT12_ZC, 0x0000);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_OUT12_ZC, 0b0);
+        wm8904_write_reg(WM8904_ANALOGUE_OUT12_ZC, 0b0);
         // wm8904_write_register(WM8904_CHARGE_PUMP_0, WM8904_CP_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_CHARGE_PUMP_0, 0b1);
+        wm8904_write_reg(WM8904_CHARGE_PUMP_0, 0b1);
         // wm8904_write_register(WM8904_CLASS_W_0, WM8904_CP_DYN_PWR);
-        write_reg(WM8904_ADDRESS, WM8904_CLASS_W_0, 0b1);
+        wm8904_write_reg(WM8904_CLASS_W_0, 0b1);
 
         // wm8904_write_register(WM8904_FLL_CONTROL_1, 0x0000);
-        // write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_1, 0b0);
+        // wm8904_write_reg(WM8904_FLL_CONTROL_1, 0b0);
         // wm8904_write_register(WM8904_FLL_CONTROL_2, WM8904_FLL_OUTDIV(7)| WM8904_FLL_FRATIO(4));
-        write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_2, (9 << 8));
+        wm8904_write_reg(WM8904_FLL_CONTROL_2, (9 << 8));
         // wm8904_write_register(WM8904_FLL_CONTROL_3, WM8904_FLL_K(0x8000));
-        write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_3, 0x0);
+        wm8904_write_reg(WM8904_FLL_CONTROL_3, 0x0);
         // wm8904_write_register(WM8904_FLL_CONTROL_4, WM8904_FLL_N(0xBB));
-        write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_4, (72 << 5));
+        wm8904_write_reg(WM8904_FLL_CONTROL_4, (72 << 5));
         // WM8904_FLL_CONTROL_5
-        write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_5, 0b1 | (0b100)); // use bitclock as FLL reference
+        wm8904_write_reg(WM8904_FLL_CONTROL_5, 0b1 | (0b100)); // use bitclock as FLL reference
         // wm8904_write_register(WM8904_FLL_CONTROL_1, WM8904_FLL_FRACN_ENA | WM8904_FLL_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_FLL_CONTROL_1, 0b001); // ENABLE FLL  FRACN_ENA false
+        wm8904_write_reg(WM8904_FLL_CONTROL_1, 0b001); // ENABLE FLL  FRACN_ENA false
         k_msleep(5);
 
         // wm8904_write_register(WM8904_CLOCK_RATES_1, WM8904_CLK_SYS_RATE(3) | WM8904_SAMPLE_RATE(5));
-        write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_1, 0b101 | (0b11 << 10)); // sysclk = 256fs, fs =~ 44.1 khz
+        wm8904_write_reg(WM8904_CLOCK_RATES_1, 0b101 | (0b11 << 10)); // sysclk = 256fs, fs =~ 44.1 khz
         // wm8904_write_register(WM8904_CLOCK_RATES_0, 0x0000);
-        // write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_0, 0b0);
+        // wm8904_write_reg(WM8904_CLOCK_RATES_0, 0b0);
         // wm8904_write_register(WM8904_CLOCK_RATES_2,
         //           		     WM8904_SYSCLK_SRC | WM8904_CLK_SYS_ENA | WM8904_CLK_DSP_ENA);
-        // write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 14)); // sysclk src = pll, disable sysclock
+        // wm8904_write_reg(WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 14)); // sysclk src = pll, disable sysclock
         // wm8904_write_register(WM8904_AUDIO_INTERFACE_1, WM8904_BCLK_DIR | WM8904_AIF_FMT_I2S);
         const int word_len = (0b00 << 2); // 16 bit
-        write_reg(WM8904_ADDRESS, WM8904_AUDIO_INTERFACE_1, 0b10 | word_len);
+        wm8904_write_reg(WM8904_AUDIO_INTERFACE_1, 0b10 | word_len);
         // wm8904_write_register(WM8904_AUDIO_INTERFACE_2, WM8904_BCLK_DIV(8));
-        // write_reg(WM8904_ADDRESS, WM8904_AUDIO_INTERFACE_2, 0b0100); // <- master only?
+        // wm8904_write_reg(WM8904_AUDIO_INTERFACE_2, 0b0100); // <- master only?
         // wm8904_write_register(WM8904_AUDIO_INTERFACE_3, WM8904_LRCLK_DIR | WM8904_LRCLK_RATE(0x20)); // <- master only?
-        // write_reg(WM8904_ADDRESS, 0x1b, xxx);
+        // wm8904_write_reg(0x1b, xxx);
         // wm8904_write_register(WM8904_POWER_MANAGEMENT_6,
         // 	         			 WM8904_DACL_ENA | WM8904_DACR_ENA |
         // 				WM8904_ADCL_ENA | WM8904_ADCR_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_POWER_MANAGEMENT_6, 0b1111);
+        wm8904_write_reg(WM8904_POWER_MANAGEMENT_6, 0b1111);
         k_msleep(5);
 
         // wm8904_write_register(WM8904_ANALOGUE_LEFT_INPUT_0, WM8904_LIN_VOL(0x10));
@@ -440,45 +448,45 @@ void init_wm8904_codec_old()
 
         // wm8904_write_register(WM8904_ANALOGUE_HP_0,
         // 				WM8904_HPL_ENA | WM8904_HPR_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, 0b10001);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, 0b10001);
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, 0b10001);
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, 0b10001);
         k_msleep(1);
         // wm8904_write_register(WM8904_ANALOGUE_HP_0,
         // 				WM8904_HPL_ENA_DLY | WM8904_HPL_ENA |
         // 				WM8904_HPR_ENA_DLY | WM8904_HPR_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, 0b110011);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, 0b110011);
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, 0b110011);
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, 0b110011);
 
         // wm8904_write_register(WM8904_DC_SERVO_0,
         // WM8904_DCS_ENA_CHAN_3 | WM8904_DCS_ENA_CHAN_2 |
         // WM8904_DCS_ENA_CHAN_1 | WM8904_DCS_ENA_CHAN_0);
-        write_reg(WM8904_ADDRESS, WM8904_DC_SERVO_0, 0b1111);
+        wm8904_write_reg(WM8904_DC_SERVO_0, 0b1111);
         // wm8904_write_register(WM8904_DC_SERVO_1,
         // 			WM8904_DCS_TRIG_STARTUP_3 | WM8904_DCS_TRIG_STARTUP_2 |
         // 		WM8904_DCS_TRIG_STARTUP_1 | WM8904_DCS_TRIG_STARTUP_0);
-        write_reg(WM8904_ADDRESS, WM8904_DC_SERVO_1, (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7));
+        wm8904_write_reg(WM8904_DC_SERVO_1, (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7));
         k_msleep(100);
         // wm8904_write_register(WM8904_ANALOGUE_HP_0,
         // 			WM8904_HPL_ENA_OUTP | WM8904_HPL_ENA_DLY | WM8904_HPL_ENA |
         // 		WM8904_HPR_ENA_OUTP | WM8904_HPR_ENA_DLY | WM8904_HPR_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, 0b1110111);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, 0b1110111);
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, 0b1110111);
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, 0b1110111);
 
         // wm8904_write_register(WM8904_ANALOGUE_HP_0,
         // 			WM8904_HPL_RMV_SHORT | WM8904_HPL_ENA_OUTP | WM8904_HPL_ENA_DLY | WM8904_HPL_ENA |
         // 		WM8904_HPR_RMV_SHORT | WM8904_HPR_ENA_OUTP | WM8904_HPR_ENA_DLY | WM8904_HPR_ENA);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_HP_0, 0b11111111);
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_LINEOUT_0, 0b11111111);
+        wm8904_write_reg(WM8904_ANALOGUE_HP_0, 0b11111111);
+        wm8904_write_reg(WM8904_ANALOGUE_LINEOUT_0, 0b11111111);
 
         // wm8904_write_register(WM8904_ANALOGUE_OUT1_LEFT, WM8904_HPOUT_VU | WM8904_HPOUTL_VOL(0x39));
         // wm8904_write_register(WM8904_ANALOGUE_OUT1_RIGHT, WM8904_HPOUT_VU | WM8904_HPOUTR_VOL(0x39));
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_OUT1_LEFT, 0b111001 | (1 << 7));
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_OUT1_RIGHT, 0b111001 | (1 << 7));
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_OUT2_LEFT, 0b111001 | (1 << 7));
-        write_reg(WM8904_ADDRESS, WM8904_ANALOGUE_OUT2_RIGHT, 0b111001 | (1 << 7));
+        wm8904_write_reg(WM8904_ANALOGUE_OUT1_LEFT, 0b111001 | (1 << 7));
+        wm8904_write_reg(WM8904_ANALOGUE_OUT1_RIGHT, 0b111001 | (1 << 7));
+        wm8904_write_reg(WM8904_ANALOGUE_OUT2_LEFT, 0b111001 | (1 << 7));
+        wm8904_write_reg(WM8904_ANALOGUE_OUT2_RIGHT, 0b111001 | (1 << 7));
 
         k_msleep(100);
 
-        write_reg(WM8904_ADDRESS, WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 2) | (1 << 14)); // sysclk src = pll, ENABLE CLK_SYS_ENA
+        wm8904_write_reg(WM8904_CLOCK_RATES_2, (1 << 1) | (1 << 2) | (1 << 14)); // sysclk src = pll, ENABLE CLK_SYS_ENA
     }
 }
